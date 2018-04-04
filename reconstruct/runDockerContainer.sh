97d538ed5f48396300901bdb3e4840fcf5546362
Xavier Corbillon <xavier.corbillon@imt-atlantique.fr>
2018-04-04 10:22:00 +0200

    Add link to download the encoded videos

--- reconstruct/runDockerContainer.sh
+++ reconstruct/runDockerContainer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ ! -d "dash_repr" ]; then
+    echo "Download the video dataset"
+    curl -C - -o mv360videos_mmsys18.tar http://dash.ipv6.enstb.fr/headMovements/mv360videos_mmsys18.tar
+    tar xfva mv360videos_mmsys18.tar
+fi
 docker run --rm -t -i --name recontruct_tiled_mpv360_mmsys18 -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/home/reconstruct -v /tmp/trans360 xmar/recontruct_tiled_mpv360_mmsys18 ./reconstruct_viewport_quality.py ${@:1:99}
